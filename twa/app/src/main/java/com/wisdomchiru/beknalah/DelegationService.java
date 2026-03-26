27241427cf32b7a54dad3ccc64a989d2b83a3293
wisdomchiru9-png <wisdomchiru9@gmail.com>
2026-03-26 21:38:12 +0530

    Add TWA Android project

---
+++ twa/app/src/main/java/com/wisdomchiru/beknalah/DelegationService.java
@@ -0,0 +1,14 @@
+package com.wisdomchiru.beknalah;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
